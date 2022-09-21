1ddbcd4d92f70d9e9c18c16235db32749097a4ae
Mattia De Prisco <mattia.deprisco@gru.bitron-ind.com>
2022-09-21 17:11:50 +0200

    Add conanfile for package generation

---
+++ conanfile.py
@@ -0,0 +1,53 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+
+class SpdlogMicroUpConan(ConanFile):
+    name = "spdlog_micro_up"
+    version = "1.10"
+
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "header_only": [True, False],
+        "wchar_support": [True, False],
+        "wchar_filenames": [True, False],
+        "no_exceptions": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "header_only": False,
+        "wchar_support": False,
+        "wchar_filenames": False,
+        "no_exceptions": False,
+    }
+
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.components["libspdlog"].names["pkg_config"] = "libspdlog"
+        self.cpp_info.components["libspdlog"].libs = ["spdlog"]
+        self.cpp_info.components["libspdlog"].libdirs = []
+        self.cpp_info.components["libspdlog"].include_dirs = []
